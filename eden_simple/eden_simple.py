9edea9294b169ce8f326ae255e88cc734ae137fc
lorentzo <lovro.bosnar1@gmail.com>
2019-03-19 10:19:06 +0100

    eden enhance

--- eden_simple/eden_simple.py
+++ eden_simple/eden_simple.py
@@ -1,75 +1,114 @@
+""" **********************************************************************************
+IMPORTS
+*********************************************************************************** """
 import numpy as np
 import time 
 import matplotlib.pyplot as plt
 
-def find_grow_sites(plate, populated):
 
-    found_grow_sites = []
+""" **********************************************************************************
+IMPROVE:
 
-    "check around populated cells"
-    for pop in populated:
+    FEATURES:
+        + color different clusters
+        + CLI input
+********************************************************************************** """
 
-            i = pop[0]
-            j = pop[1]
+""" ***********************************************************************************
+CLASS
+lattice EDEN model implementation
+Start from arbitrary number of cells 
+4-neigh growth
+************************************************************************************* """
 
-            "if stumpled upon occupied place look for 8 neigh and find free cells"
-            if plate[i,j] == 1:
+class EDEN:
 
-                if plate[i+1, j+1] == 0:
-                    found_grow_sites.append((i+1, j+1))
+    """ ******************************************************************************
+    CONSTRUCTOR
+    plate_size: [x_max, y_max]
+    n_iter: scalar
+    starters: [[x01, y01], [x02, y02], ....]
+    ******************************************************************************* """
+    def __init__(self, plate_size, n_iter, starters):
 
-                if plate[i+1, j-1] == 0:
-                    found_grow_sites.append((i+1, j-1))
+        "User specified variables"
+        self.plate_size = plate_size
+        self.n_iter = n_iter
+        self.starters = starters
 
-                if plate[i-1, j+1] == 0:
-                    found_grow_sites.append((i-1, j+1))
+        "Additional variables"
+        self.plate = np.zeros(self.plate_size)
+        self.populated = []
+        for starter in self.starters:
+            self.plate[starter[0]][starter[1]] = 1
+            self.populated.append([starter[0], starter[1]])
 
-                if plate[i-1, j-1] == 0:
-                    found_grow_sites.append((i-1, j-1))
+        
+    """ ***************************************************************************
+    PRIVATE
+    Helper function: finds free slots around populated cells, 4-neigh
+    ***************************************************************************** """
+    def find_grow_sites(self):
 
-                if plate[i, j+1] == 0:
-                    found_grow_sites.append((i, j+1))
+        found_grow_sites = set()
 
-                if plate[i, j-1] == 0:
-                    found_grow_sites.append((i, j-1))
+        "check around populated cells"
+        for cell in self.populated:
 
-                if plate[i-1, j] == 0:
-                    found_grow_sites.append((i-1, j))
+                i = cell[0]
+                j = cell[1]
 
-                if plate[i-1, j] == 0:
-                    found_grow_sites.append((i-1, j))
+                if self.plate[i, j+1] == 0:
+                    found_grow_sites.add((i, j+1))
 
-    "remove duplicates!"
-    return found_grow_sites
+                if self.plate[i, j-1] == 0:
+                    found_grow_sites.add((i, j-1))
 
-                
+                if self.plate[i-1, j] == 0:
+                    found_grow_sites.add((i-1, j))
+
+                if self.plate[i+1, j] == 0:
+                    found_grow_sites.add((i+1, j))
+
+        return list(found_grow_sites)
+
+
+    """ ****************************************************************************
+    PUBLIC
+    call to grow eden pattern
+    **************************************************************************** """
+    def grow_pattern(self):
+
+        for sample in range(self.n_iter):
+
+            "find coordinates where next cell can be spawned"
+            grow_sites = self.find_grow_sites()
+
+            "among found coordinates choose randomly one"
+            next_grow_site_idx = np.random.randint(low=0, high=len(grow_sites), size=1)[0]
+            next_grow_site_coord = grow_sites[next_grow_site_idx]
+
+            "occupy chosen cell"
+            self.plate[next_grow_site_coord[0]][next_grow_site_coord[1]] = 1
+            self.populated.append([next_grow_site_coord[0], next_grow_site_coord[1]])
+
+        plt.imshow(self.plate)
+        plt.show()
+
+
+""" *************************************************************************************
+MAIN
+Allow user to specify attributes in EDEN class
+Create EDEN class object
+Run
+************************************************************************************* """            
 def main():
   
-  plate = np.zeros((1000,1000))
-  n_iter = 4000
-  populated = [(300,300), (700, 700)]
+  eden = EDEN([500,500], 5000, [[250,250], [300,300]])
+  eden.grow_pattern()
 
-  for pop in populated:
-      plate[pop[0], pop[1]] = 1
-
-  cnt_iter = 0
-  while cnt_iter < n_iter:
-
-      "Returns coordinates where next cell could be generated: [(x1,y1),...]"
-      grow_sites = find_grow_sites(plate, populated)
-      
-      "choose one grow site at random"
-      next_grow_site_idx = np.random.randint(low=0, high=len(grow_sites), size=1)[0]
-      next_grow_site_coord = grow_sites[next_grow_site_idx]
-
-      "occupy chosen grow site"
-      populated.append(next_grow_site_coord)
-      plate[next_grow_site_coord[0], next_grow_site_coord[1]] = 1
-
-      cnt_iter = cnt_iter + 1
-
-  plt.imshow(plate)
-  plt.show()
-
-
-main()
+""" **************************************************************************************
+ROOT
+************************************************************************************** """
+if __name__ == '__main__':
+    main()
