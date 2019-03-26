0109ad9cf0a02b02fa8de9abac2dfc7014070440
lorentzo <lovro.bosnar1@gmail.com>
2019-03-26 12:40:02 +0100

    SCA for blender + rendering

--- blender_implementations/cetvrti/test.py
+++ blender_implementations/cetvrti/test.py
@@ -24,9 +24,9 @@ class Branch():
         next_branch = Branch(next_position, self, copy.copy(self.direction))
         return next_branch
 
-    def show(self):
+    def show(self, mball):
 
-        n_samples = 2
+        n_samples = 1
         delta_t = 1 / n_samples
 
         if self.parent != None:
@@ -36,7 +36,10 @@ class Branch():
 
                 pos = (1-t) * np.array(self.parent.position) + t * np.array(self.position)
 
-                bpy.ops.mesh.primitive_uv_sphere_add(location=pos, size=0.4, segments=5)
+                #bpy.ops.mesh.primitive_uv_sphere_add(location=pos, size=0.4, segments=5)
+                element = mball.elements.new()
+                element.co = pos
+                element.radius = 1.0
 
                 t += delta_t
 
@@ -47,7 +50,7 @@ class Branch():
 class Leaf():
 
     def __init__(self):
-        self.position = np.random.rand(3) * 50
+        self.position = np.random.rand(3) * 70 - 35
         self.position[2] = np.random.rand(1)[0]*5
         self.reached = False
 
@@ -57,32 +60,54 @@ class Leaf():
 
 class Tree():
 
-    def __init__(self):
+    def __init__(self, root_position, id):
 
         self.n_leaves = 150
+        self.root_position = root_position
+        self.id = id
         
         self.max_dist = 5
         self.min_dist = 2
         
         self.leaves = []
         
-        
         self.branches = []
 
         for i in range(self.n_leaves):
             self.leaves.append(Leaf())
 
+        # create root of the tree (point without parent) and turn its direction to leafs
+        self.root = Branch(self.root_position, None, None)
+        closest_leaf_to_root = self.find_closest_leaf_to_branch(self.root)
+        root_direction = np.array(closest_leaf_to_root.position) - np.array(self.root.position)
+        root_direction /= np.linalg.norm(root_direction)
+        self.root.direction = root_direction
+
+        # add root to branches
+        self.branches.append(self.root)
+
+
+    def find_closest_leaf_to_branch(self, branch):
+
+        # random ini
+        closest_dist = math.inf
+        closest_leaf = self.leaves[0]
+
+        for leaf in self.leaves:
+
+            dist = np.linalg.norm(np.array(leaf.position)-np.array(branch.position))
+
+            if dist < closest_dist:
+                closest_dist = dist
+                closest_leaf = leaf
+
+        return closest_leaf
+
 
     def grow_to_point_cloud(self):
 
-        # create root of the tree (point without parent)
-        root = Branch([-10,-10,-10], None, [0.25,0.25,0.25])
-
-        # add root to branches
-        self.branches.append(root)
-
         # grow the branch to point cloud
-        curr_branch = root 
+        curr_branch = self.root 
         found = False
 
         while not found:
@@ -90,72 +115,90 @@ class Tree():
             for i in range(self.n_leaves):
 
                 dist = np.linalg.norm(np.array(self.leaves[i].position)-np.array(curr_branch.position))
-                print(dist)
 
                 if dist < self.max_dist:
                     found = True
 
             # create a new branch
             if not found:
+
                 branch = curr_branch.give_next_branch()
-                #bpy.ops.mesh.primitive_cube_add(location=branch.position, radius=0.4)
                 curr_branch = branch
                 self.branches.append(curr_branch)
 
     def grow_through_point_cloud(self):
 
-        # grow
-        for leaf in self.leaves:
+        n_reached = 0
 
-            if leaf.reached == True:
-                continue
+        while n_reached < self.n_leaves:
 
-            closest_branch = None
-            record = math.inf
+            # grow
+            for leaf in self.leaves:
 
-            for branch in self.branches:
+                if leaf.reached == True:
+                    continue
 
-                dist = np.linalg.norm(np.array(leaf.position) - np.array(branch.position))
+                closest_branch = None
+                record = math.inf
 
-                if dist < self.min_dist:
+                for branch in self.branches:
 
-                    leaf.riched = True
-                    closest_branch = None
-                    break
+                    dist = np.linalg.norm(np.array(leaf.position) - np.array(branch.position))
 
-                elif closest_branch == None or dist < record:
-                    closest_branch = branch
-                    record = dist
+                    if dist < self.min_dist:
 
-            if closest_branch != None:
+                        leaf.riched = True
+                        n_reached += 1
+                        closest_branch = None
+                        break
 
-                new_direction = np.array(leaf.position) - np.array(closest_branch.position)
-                new_direction = new_direction / np.linalg.norm(new_direction)
-                closest_branch.direction = new_direction
-                closest_branch.count += 1 
+                    elif closest_branch == None or dist < record:
+                        closest_branch = branch
+                        record = dist
 
-            #non_reached_leaves = []
-            #for leaf in self.leaves:
-            #    if leaf.reached == False:
-            #        non_reached_leaves.append(leaf)
-            #self.leaves = non_reached_leaves
+                if closest_branch != None:
 
-            for branch in self.branches:
-                if branch.count > 0:
-                    branch.direction /= branch.count
+                    new_direction = np.array(leaf.position) - np.array(closest_branch.position)
+                    new_direction = new_direction / np.linalg.norm(new_direction)
+                    closest_branch.direction = new_direction
+                    closest_branch.count += 1 
 
-                    new_position = branch.position + branch.direction
-                    new_branch = Branch(new_position, branch, copy.copy(branch.direction))
 
-                    self.branches.append(new_branch) 
+                # grow branches according to number of times extended
+                for branch in self.branches:
+                    if branch.count > 0:
+                        branch.direction /= branch.count
 
-                branch.reset()
+                        new_position = branch.position + branch.direction
+                        new_branch = Branch(new_position, branch, copy.copy(branch.direction))
 
-    def show_branches(self):
+                        self.branches.append(new_branch) 
+
+                    branch.reset()
+
+    def show_branches(self, cnt):
+
+        scene = bpy.context.scene
+        # add metaball object
+        mball = bpy.data.metaballs.new("MetaBall")
+        obj = bpy.data.objects.new("MetaBallObject", mball)
+        scene.objects.link(obj)
+
+        print(len(self.branches))
+
+        sample = 0
         
         for branch in self.branches:
 
-            branch.show()
+            sample += 1
+            
+
+            branch.show(mball)
+
+            if sample % 50 == 0:
+                cnt[0] += 1
+                bpy.context.scene.render.filepath = '/home/lovro/Documents/FER/diplomski/Growth_models/blender_implementations/cetvrti/izlazi/'+str(cnt[0])
+                bpy.ops.render.render(write_still=True)
 
     def show_leaves(self):
 
@@ -166,13 +209,41 @@ class Tree():
 
 def main():
 
-    tree = Tree()
-    #tree.show_leaves()
-    tree.grow_to_point_cloud()
+    obj_camera = bpy.data.objects["Camera"].location[0] = 0
+    obj_camera = bpy.data.objects["Camera"].location[1] = 0
+    obj_camera = bpy.data.objects["Camera"].location[2] = 100
     
-    tree.grow_through_point_cloud()
+    obj_camera = bpy.data.objects["Camera"].rotation_euler[0] = 0
+    obj_camera = bpy.data.objects["Camera"].rotation_euler[1] = 0
+    obj_camera = bpy.data.objects["Camera"].rotation_euler[2] = 0
+
+    cnt = [0]
+
+    tree1 = Tree([-10,-10,-10],1)
+
+    tree1.grow_to_point_cloud()
     
-    tree.show_branches()
+    tree1.grow_through_point_cloud()
+    
+    tree1.show_branches(cnt)
+    
+    
+    tree2 = Tree([50,50,50],2)
+    
+    tree2.grow_to_point_cloud()
+    
+    tree2.grow_through_point_cloud()
+    
+    tree2.show_branches(cnt)
+    
+    
+    tree3 = Tree([-50,50,50],3)
+    
+    tree3.grow_to_point_cloud()
+    
+    tree3.grow_through_point_cloud()
+    
+    tree3.show_branches(cnt)
     
 
 main()
