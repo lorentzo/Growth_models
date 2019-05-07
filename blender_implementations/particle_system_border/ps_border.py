0569133ae21c103f78dd83e5f90c98a5195c0cfe
lorentzo <lovro.bosnar1@gmail.com>
2019-05-07 11:00:50 +0200

    perlin ps

--- blender_implementations/particle_system_border/ps_border.py
+++ blender_implementations/particle_system_border/ps_border.py
@@ -2,6 +2,7 @@ import bpy
 import numpy as np 
 import bmesh
 import os
+from eden_perlin_ps import PerlinCircle
 
 def define_object(points, name, ps_object):
 
@@ -15,7 +16,7 @@ def define_object(points, name, ps_object):
     mesh_obj = bpy.data.objects.new(name+"_obj", mesh_data)
     bm.to_mesh(mesh_data)
     bm.free()
-
+    
     # add particle system
     mesh_obj.modifiers.new(name+"_ps", type='PARTICLE_SYSTEM')
     mesh_obj_ps = mesh_obj.particle_systems[0]
@@ -26,82 +27,60 @@ def define_object(points, name, ps_object):
     mesh_obj_ps.settings.frame_start = 1
     mesh_obj_ps.settings.frame_end = 0
     mesh_obj_ps.settings.use_emit_random = False
-    mesh_obj_ps.settings.count = n_points_per_step
+    mesh_obj_ps.settings.count = len(points)
 
     return mesh_obj # can reference data and ps
 
-center = [0, 0, 0]
-radius_max = 10
-n_steps = 5
-radii = np.linspace(0, radius_max, n_steps)
-n_points_per_step = 100
 
-points_pet_steps = []
-
-for r_idx in range(len(radii)-1):
-
-    curr_points = []
-    r_lower = radii[r_idx]
-    r_upper = radii[r_idx + 1]
-
-    for i in range(n_points_per_step):
-
-        found = False
-        while not found:
-
-            point = np.random.rand(3) * r_upper
-
-            point[2] = 0
-
-            if np.random.rand() > 0.5:
-                point[1] *= -1
-
-            if np.random.rand() > 0.5:
-                point[0] *= -1
-
-            if np.linalg.norm(point) < r_upper and np.linalg.norm(point) > r_lower:
-                curr_points.append(point)
-                found = True
-
-    points_pet_steps.append(curr_points)
-
-
-
+# define perlin circles
+ep = PerlinCircle(center=np.array([0,0,0]), 
+                    radius_range=np.array([1, 20, 2]),
+                    shape=np.array([1,1]))
+    
+perlin_layers, perlin_contour = ep.grow()
 
 # ps object
-bpy.ops.mesh.primitive_cube_add()
-dupli_cube = bpy.context.object
-
+# create texture for displacement
+bpy.data.textures.new('diplace_tex', type='VORONOI')
+# create sphere
+bpy.ops.mesh.primitive_uv_sphere_add(location=[10,10,10], size=1)
+# add displacement
+bpy.ops.object.modifier_add(type='DISPLACE')
+bpy.context.object.modifiers["Displace"].texture = bpy.data.textures["diplace_tex"]
+# reference dupli object
+dupli_obj = bpy.context.object
+dupli_obj.scale = [2,2,0.1]
 
 # add ps to all layers
-layers = []
+ps_layers = []
 cnt = 1
-for points in points_pet_steps:
-    layer = define_object(points, "layer"+str(cnt), dupli_cube)
+for points in perlin_contour:
+    layer = define_object(points, "layer"+str(cnt), dupli_obj)
     bpy.context.scene.objects.link(layer)
-    layers.append(layer)
+    ps_layers.append(layer)
     cnt += 1
 
 # render
+# configure camera position and orientation
+bpy.data.objects["Camera"].location = (0, 0, 50)
+bpy.data.objects["Camera"].rotation_euler = (0,0,0)
+
 iter_grow = 10
-iter_die = 10
-delta_change = 0.01
+delta_change = 0.02
 render_iter = 1
 render_out = '/home/lovro/Documents/FER/diplomski/growth_models_results/blender_impl/ps_border/tmp/'
-for layer in layers:
-    print("!")
+for layer_idx in range(len(ps_layers)):
 
+    ps_layer = ps_layers[layer_idx]
+    perlin_layer = perlin_layers[layer_idx]
+
+    # add perlin layer
+    bpy.context.scene.objects.link(perlin_layer)
+    
+    # increase ps layer
     for iter in range(iter_grow):
-        layer.particle_systems[0].settings.particle_size += delta_change
 
-        # render
-        bpy.context.scene.render.filepath = os.path.join(render_out, str(render_iter))
-        bpy.ops.render.render(write_still=True)
-
-        render_iter += 1
-
-    for iter in range(iter_die):
-        layer.particle_systems[0].settings.particle_size -= delta_change
+        ps_layer.particle_systems[0].settings.particle_size += delta_change
 
         # render
         bpy.context.scene.render.filepath = os.path.join(render_out, str(render_iter))
